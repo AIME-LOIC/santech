d01c27ec128c9ad99eaf43502a298907a3d45063
AIME-LOIC <kingaime132@gmail.com>
2026-04-06 08:45:50 +0200

    user attribute added

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -19,9 +19,22 @@ User.init({
         allowNull: true,
         unique: true,
     },
+    password: {
+        type:DataTypes.STRING,
+        allowNull: true,
+    },
+
     PhoneNumber:{
         type: DataTypes.STRING,
         allowNull: true,
+    },
+    location: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    role: {
+        type: DataTypes.ENUM('patient', 'doctor', 'admin'),
+        allowNull: false,
     },
     createdAt: {
         type: DataTypes.DATE,
